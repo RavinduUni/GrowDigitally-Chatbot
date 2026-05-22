beea038480bfa07215cace748471873a13542591
RavinduUni <fc115680@foc.sjp.ac.lk>
2026-05-22 13:45:40 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+let isConnected = false;
+const ensureDB = async () => {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+};
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(async (req, res, next) => {
+  try {
+    await ensureDB();
+    next();
+  } catch (err) {
+    res.status(500).json({ success: false, error: "Database connection failed" });
+  }
+});
+
+app.get("/", (req, res) => {
+    res.send("<h1>ChatBot Application</h1> <p>Server is running successfully</p> <a href='https://github.com/'>GitHub</a> ");
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
